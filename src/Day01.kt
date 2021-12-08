71bdd3fe93ee7bff9936a0978cf891426f92a96f
Ankita Shome <ankita.shome@kambi.com>
2021-12-08 17:51:09 +0100

    Added logic for Day01 part1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,18 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return input
+            .windowed(2)
+            .count { (a, b) -> b > a }
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val sonarReport = readInputInInt("Day01")
+    println(part1(sonarReport))
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInputInInt("Day01_test")
+    check(part1(testInput) == 10)
 }
