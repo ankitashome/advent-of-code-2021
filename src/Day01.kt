11cc7e6d6f023782436ba096073d277e8394d795
Ankita Shome <ankita.shome@kambi.com>
2021-12-09 12:30:53 +0100

    Added logic for Day01 part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -5,14 +5,20 @@ fun main() {
             .count { (a, b) -> b > a }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        return input
+            .windowed(3)
+            .map { it.sum() }
+            .windowed(2)
+            .count { (a, b) -> b > a }
     }
 
     val sonarReport = readInputInInt("Day01")
     println(part1(sonarReport))
+    println(part2(sonarReport))
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInputInInt("Day01_test")
-    check(part1(testInput) == 10)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 }
